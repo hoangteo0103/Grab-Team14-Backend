761d1afcbe57dafe3353b46ea7ae18062718bf7e
hoang.bn <51512556+hoangteo0103@users.noreply.github.com>
2024-05-06 20:21:46 +0700

    Force run

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
     .addTag('Emploi')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('API', app, document);
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
